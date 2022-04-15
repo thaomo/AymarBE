807e19aafe2e72840cee39b47cce5067148df0aa
Mo <mthao09.mt@gmail.com>
2022-04-15 12:24:05 -0500

    update server.js and package.json

--- server.js
+++ server.js
@@ -6,13 +6,21 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 
-let mongodb_pass = process.env.MONGODB_PASS;
+app.get('/', (req, res) => {
+    res.send('hello world!');
+})
+
+app.get('/greeting', (req, res) => {
+    res.json({ greetings: 'hello' });
+})
+
+//let mongodb_pass = process.env.MONGODB_PASS;
 
 //connect to mongodb
-mongoose.connect(`mongodb+srv://thaomo:` + mongodb_pass + `@cluster0.f6ccp.mongodb.net/grantForms`)
+//mongoose.connect(`mongodb+srv://thaomo:` + mongodb_pass + `@cluster0.f6ccp.mongodb.net/grantForms`)
 
 //require routes
-app.use("/", require("./routes/formRoute"));
+//app.use("/", require("./routes/formRoute"));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("express server is running on port" + process.env.PORT);
