3b6988cd0f412881b23e6ce98eb890950a4251ea
Mo <mthao09.mt@gmail.com>
2022-04-15 12:28:05 -0500

    connect to mongodb again

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('hello world!');
-})
+// For Testing only
+// app.get('/', (req, res) => {
+//     res.send('hello world!');
+// })
 
-app.get('/greeting', (req, res) => {
-    res.json({ greetings: 'hello' });
-})
+// app.get('/greeting', (req, res) => {
+//     res.json({ greetings: 'hello' });
+// })
 
-//let mongodb_pass = process.env.MONGODB_PASS;
+let mongodb_pass = process.env.MONGODB_PASS;
 
 //connect to mongodb
-//mongoose.connect(`mongodb+srv://thaomo:` + mongodb_pass + `@cluster0.f6ccp.mongodb.net/grantForms`)
+mongoose.connect(`mongodb+srv://thaomo:` + mongodb_pass + `@cluster0.f6ccp.mongodb.net/grantForms`)
 
 //require routes
-//app.use("/", require("./routes/formRoute"));
+app.use("/", require("./routes/formRoute"));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("express server is running on port" + process.env.PORT);
