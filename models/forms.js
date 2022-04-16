1eeada08e48dff47cd858a70ae5fdf51265984ea
thaomo <43796992+thaomo@users.noreply.github.com>
2022-04-16 09:24:20 -0500

    add fields to the schema and routes

--- models/forms.js
+++ models/forms.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
 
 const formsSchema = {
+    lName: String,
+    fName: String,
+    age: Number,
+    address: String,
+    gender: String,
+    currentSchool: String,
+    gpa: Number,
+    phone: String,
     email: String,
+    teacherName: String,
+    grantsApplyingFor: String,
+    sportsInvolved: String,
+    listOfSports: String,
+    coachName: String,
     message: String,
+
 }
 
 const Form = mongoose.model("forms", formsSchema);
--- routes/formRoute.js
+++ routes/formRoute.js
@@ -3,13 +3,39 @@ const router = express.Router();
 const Form = require("../models/forms");
 
 router.route("/create").post((req, res) => {
+    const lName = req.body.lName;
+    const fName = req.body.fName;
+    const age = req.body.age;
+    const address = req.body.address;
+    const gender = req.body.gender;
+    const currentSchool = req.body.currentSchool;
+    const gpa = req.body.gpa;
+    const phone = req.body.phone;
     const email = req.body.email;
+    const teacherName = req.body.teacherName;
+    const grantsApplyingFor = req.body.grantsApplyingFor;
+    const sportsInvolved = req.body.sportsInvolved;
+    const listOfSports = req.body.listOfSports;
+    const coachName = req.body.coachName;
     const message = req.body.message;
+
     const newForm = new Form({
+        lName,
+        fName,
+        age,
+        address,
+        gender,
+        currentSchool,
+        gpa,
+        phone,
         email,
+        teacherName,
+        grantsApplyingFor,
+        sportsInvolved,
+        listOfSports,
+        coachName,
         message,
     });
-
     newForm.save();
 });
 
